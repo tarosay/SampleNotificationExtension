b158dd88cb2522208e2b889f63436ccf1b0bdfa0
Tarosa <tarosa.y@gmail.com>
2012-04-16 18:59:47 +0900

    first commit

---
+++ gen/com/sonyericsson/extras/liveware/extension/notificationsample/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.sonyericsson.extras.liveware.extension.notificationsample;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
